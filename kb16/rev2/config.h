90cf4f4a197d9ecdf20becd5b37257fc03c47bdf
Anton Hang <hang.anthony@gmail.com>
2023-01-10 00:09:55 -0500

    Update config.h

--- kb16/rev2/config.h
+++ kb16/rev2/config.h
@@ -44,7 +44,8 @@
     #define I2C1_SCL_PIN B10
     #define I2C1_SDA_PIN B11
     #define I2C_DRIVER I2CD2
-
+    /* OLED always on*/
+    #define OLED_DISABLE_TIMEOUT
     /* Use the custom font */
     #define OLED_FONT_H "./lib/glcdfont.c"
 #endif
